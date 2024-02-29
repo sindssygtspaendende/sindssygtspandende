4602c58f8ec5e1c39193287451a588db53fb41d6
sindssygtspaendende <157467753+sindssygtspaendende@users.noreply.github.com>
2024-02-29 11:06:56 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["👑 I am RTX","Spotify","Youtube","SUBSCRIBE❤️","THANKS 🌇"];
+const statusMessages = [" "," "," "," "," "];
 
 
 let currentIndex = 0;
